224a0f1b4a3f6cc32df4ea7b6d2bb3e4a5b7384b
jvcalassio <joao.victors1203@gmail.com>
2018-11-27 08:48:20 -0200

    t2

--- t2/add_sub.v
+++
@@ -1,9 +0,0 @@
-module add_sub(A, B, CIN, M, F, COUT);
-  input [3:0]A,B;
-  input CIN,M;
-  output [3:0]F;
-  output COUT;
-  wire [3:0]W;
-  complement_generator comgen0(B,M,W);
-  bcd_adder add0(A, W, CIN, F, COUT);
-endmodule
